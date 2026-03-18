5f7c8c87662a587e2adb858c95eb7938cfaa2680
Vinicius Marins de Souza <826172031@ulife.com.br>
2026-03-17 21:23:05 -0300

    aula t03 calvetti

---
+++ Github.java
@@ -0,0 +1,8 @@
+public class Github {
+   public static void main (String[] args) {
+     String nome = "Ryann" ;
+     int idade = 19 ;
+      double altura = 1.79 ;
+       System.out.println ("nome: " + nome + " idade: " + idade + " altura: " + altura );
+    }
+ }
